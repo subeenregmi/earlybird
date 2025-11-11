96ffd76c8af451cfc9d3456dbef41b1a5159af31
Subeen Regmi <subeenregmi123@gmail.com>
2025-11-11 06:26:04 +0000

    feat: use time

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::time::{self, UNIX_EPOCH};
+
 fn main() {
-    println!("Hello, world!");
+    let now = time::SystemTime::now().duration_since(UNIX_EPOCH);
+
+    match now {
+        Ok(n) => println!("The time now is {}", n.as_secs()),
+        Err(_) => println!("The time is not valid"),
+    }
 }
