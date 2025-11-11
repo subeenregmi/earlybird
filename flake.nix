a595ddbffdda69a9ba4a287e9dee8a1e50a8785b
Subeen Regmi <subeenregmi123@gmail.com>
2025-11-11 05:48:17 +0000

    feat: flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "earlybird rust flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let 
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "earlybird";
+
+        packages = with pkgs; [
+          rustc 
+          cargo
+        ];
+
+      };
+    };
+}
