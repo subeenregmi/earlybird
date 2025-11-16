14159d32147dc164d898f2b468f444d6c8d0ae56
Subeen Regmi <subeenregmi123@gmail.com>
2025-11-16 13:47:58 +0000

    feat: check for dir and file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,56 @@
-use std::time::{self, UNIX_EPOCH};
+use std::{
+    fs::{self, File, create_dir},
+    io::Read,
+    path::Path,
+};
+
+const DATA_DIR: &str = "/home/subeen/.local/share/earlybird/";
+const DATA_FILE: &str = "data.csv";
 
 fn main() {
-    let now = time::SystemTime::now().duration_since(UNIX_EPOCH);
+    let dir_path = Path::new(DATA_DIR);
 
-    match now {
-        Ok(n) => println!("The time now is {}", n.as_secs()),
-        Err(_) => println!("The time is not valid"),
+    let dir_exists = match dir_path.try_exists() {
+        Ok(b) => b,
+        Err(e) => panic!("error trying to check if dir path exists, {}", e),
+    };
+
+    if !dir_exists {
+        if let Err(e) = fs::create_dir(dir_path) {
+            panic!("error trying to create new dir, {}", e)
+        }
     }
+    let mut file: File;
+
+    let mut file_path_str = String::from(DATA_DIR);
+    file_path_str.push_str(DATA_FILE);
+
+    let file_path = Path::new(&file_path_str);
+
+    let file_exists = match file_path.try_exists() {
+        Ok(b) => b,
+        Err(e) => panic!("error trying to check if file exists {}", e),
+    };
+
+    if file_exists {
+        println!("file exists!");
+        file = match File::open(file_path) {
+            Ok(f) => f,
+            Err(e) => panic!("error trying to open file, {}", e),
+        }
+    } else {
+        println!("file has just been created!");
+        file = match File::create_new(file_path) {
+            Ok(f) => f,
+            Err(e) => panic!("error trying to create new file, {}", e),
+        }
+    }
+
+    let mut buf = String::new();
+
+    if let Err(e) = file.read_to_string(&mut buf) {
+        panic!("error trying to read file, {}", e)
+    }
+
+    println!("File contents: {}", buf)
 }
