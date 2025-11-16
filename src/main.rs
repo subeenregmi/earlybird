e2f7b202f8c0d929ccf803cd1d869dd7a1bfe068
Subeen Regmi <subeenregmi123@gmail.com>
2025-11-16 17:22:32 +0000

    feat: added printing a calendar

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use std::{
-    fs::{self, File, create_dir},
-    io::Read,
+    fs::{File, create_dir},
+    io::{Read, Seek, Write},
     path::Path,
 };
 
+use chrono::{DateTime, Datelike, Local, Month, Weekday};
+
+#[derive(Debug)]
+struct LoggedTimes {
+    times: Vec<DateTime<Local>>,
+}
+
 const DATA_DIR: &str = "/home/subeen/.local/share/earlybird/";
-const DATA_FILE: &str = "data.csv";
+const DATA_FILE: &str = "data";
 
 fn main() {
     let dir_path = Path::new(DATA_DIR);
@@ -16,7 +23,7 @@ fn main() {
     };
 
     if !dir_exists {
-        if let Err(e) = fs::create_dir(dir_path) {
+        if let Err(e) = create_dir(dir_path) {
             panic!("error trying to create new dir, {}", e)
         }
     }
@@ -33,24 +40,92 @@ fn main() {
     };
 
     if file_exists {
-        println!("file exists!");
-        file = match File::open(file_path) {
+        file = match File::options().read(true).write(true).open(file_path) {
             Ok(f) => f,
             Err(e) => panic!("error trying to open file, {}", e),
         }
     } else {
-        println!("file has just been created!");
         file = match File::create_new(file_path) {
             Ok(f) => f,
             Err(e) => panic!("error trying to create new file, {}", e),
         }
     }
 
-    let mut buf = String::new();
+    let mut times_buf = String::new();
 
-    if let Err(e) = file.read_to_string(&mut buf) {
+    if let Err(e) = file.read_to_string(&mut times_buf) {
         panic!("error trying to read file, {}", e)
     }
 
-    println!("File contents: {}", buf)
+    let mut logged_times = LoggedTimes { times: Vec::new() };
+
+    for time in times_buf.split("\n") {
+        if time == "" {
+            continue;
+        }
+        match time.parse::<DateTime<Local>>() {
+            Ok(t) => logged_times.times.push(t),
+            Err(e) => panic!("error parsing time entry, {}, {}", time, e),
+        }
+    }
+
+    let mut current_entry = Local::now().to_string();
+    current_entry.push('\n');
+
+    if let Err(e) = file.write(current_entry.as_bytes()) {
+        panic!("error trying to write to file, {}", e)
+    }
+
+    if let Err(e) = file.flush() {
+        panic!("error trying to flush file contents, {}", e)
+    }
+
+    let current_time = Local::now();
+
+    let month_num = match u8::try_from(current_time.month0()) {
+        Ok(n) => n,
+        Err(e) => panic!("error trying to turn month into u8, {}", e),
+    };
+
+    let month = match Month::try_from(month_num) {
+        Ok(m) => m,
+        Err(e) => panic!("error trying to turn month u8 to month, {}", e),
+    };
+
+    let day_num = match u8::try_from(current_time.day()) {
+        Ok(n) => n,
+        Err(e) => panic!("error trying to turn day into u8, {}", e),
+    };
+
+    let left_padding = ((4 * 7) / 2) - (month.name().len() / 2);
+    println!("{:<left_padding$}{}", "", month.name());
+
+    let days: [Weekday; 7] = [
+        Weekday::Mon,
+        Weekday::Tue,
+        Weekday::Wed,
+        Weekday::Thu,
+        Weekday::Fri,
+        Weekday::Sat,
+        Weekday::Sun,
+    ];
+
+    for day in days {
+        print!("{} ", day)
+    }
+
+    println!();
+
+    for i in 1..=current_time.num_days_in_month() {
+        if i == day_num {
+            print!("\x1b[1;91m{i:<4}\x1b[0m")
+        } else {
+            print!("{i:<4}");
+        }
+        if i % 7 == 0 {
+            println!()
+        }
+    }
+
+    println!()
 }
